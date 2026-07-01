d2147102635cee3ee1ba83dc17fe798641d0fa0d
xingbin deng <dengxingbin.dev@gmail.com>
2026-07-01 11:27:34 +0800

    first commit

--- demo_0/ViewController.swift
+++ demo_0/ViewController.swift
@@ -1,19 +1,36 @@
 //
 //  ViewController.swift
-//  demo_0
+//  Example_01
 //
-//  Created by Dengxingbin on 2026/7/1.
+//  Created by Meetfire on 2024/11/10.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello World!"
+        label.font = UIFont.systemFont(ofSize: 18.0, weight: .semibold)
+        label.textColor = UIColor.red
+        label.backgroundColor = UIColor.black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override func viewDidLoad() {   
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupUI()
+    }
+    
+    private func setupUI() {
+        self.view.backgroundColor = UIColor.systemBlue.withAlphaComponent(0.5)
+        self.view.addSubview(self.textLabel)
+        self.setupConstraints()
     }
 
-
+    private func setupConstraints() {
+        self.textLabel.frame = .init(x: 0, y: 0, width: 300, height: 100)
+        self.textLabel.center = self.view.center
+    }
 }
-
